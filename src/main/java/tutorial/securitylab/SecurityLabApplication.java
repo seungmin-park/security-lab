d8bff2095fb9b9592a217512cb0044c5433fe429
박승민 <tmddudals369@naver.com>
2023-07-25 23:48:07 +0900

    feat: create security-lab project

---
+++ src/main/java/tutorial/securitylab/SecurityLabApplication.java
@@ -0,0 +1,12 @@
+package tutorial.securitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityLabApplication.class, args);
+    }
+}
