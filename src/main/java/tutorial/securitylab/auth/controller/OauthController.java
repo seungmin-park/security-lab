d29e91e8826daee6706b2e6623fe59f97c0364ba
박승민 <tmddudals369@naver.com>
2023-07-26 17:46:18 +0900

    refactor: 패키지 변경

--- src/main/java/tutorial/securitylab/auth/controller/OauthController.java
+++ src/main/java/tutorial/securitylab/auth/controller/OauthController.java
@@ -1,4 +1,4 @@
-package tutorial.securitylab.auth.dto;
+package tutorial.securitylab.auth.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/tutorial/securitylab/auth/dto/OauthAccessTokenRequest.java
+++ src/main/java/tutorial/securitylab/auth/dto/OauthAccessTokenRequest.java
@@ -1,4 +1,4 @@
-package tutorial.securitylab.auth.controller;
+package tutorial.securitylab.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
